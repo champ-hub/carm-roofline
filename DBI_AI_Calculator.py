6787a78384e5e34cb3e14ecf3c41182eb84381f6
zemorg <zemor.morgado@gmail.com>
2024-05-25 03:27:09 +0100

    Improved mixed benchmarks, better output, bug fixes

--- DBI_AI_Calculator.py
+++ DBI_AI_Calculator.py
@@ -1,8 +1,12 @@
 import argparse
 import subprocess
 import os
-import matplotlib.pyplot as plt
-import numpy as np
+plot_numpy = 1
+try:
+    import matplotlib.pyplot as plt
+    import numpy as np
+except ImportError:
+    plot_numpy = None
 import datetime
 import time
 import re
@@ -1087,9 +1091,12 @@ if __name__ == "__main__":
 
     #Plot Roofline
     if args.drawroof:
-        print("Manual application plotting not implemented iet")
-        #date = ct.strftime('%Y-%m-%d_%H-%M-%S')
-        #plot_roofline_with_dot(args.executable_path, gflops, ai, args.choice, args.roi, date)
+        if not plot_numpy == None:
+            print("Manual application plotting not implemented iet, results can be viewed using the GUI")
+            #date = ct.strftime('%Y-%m-%d_%H-%M-%S')
+            #plot_roofline_with_dot(args.executable_path, gflops, ai, args.choice, args.roi, date)
+        else:
+            print("No Matplotlib and/or Numpy found, in order to draw CARM graphs make sure to install them.")
     date = ct.strftime('%Y-%m-%d %H:%M:%S')
     
 
